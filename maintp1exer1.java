a13036eee7849dfdc93e9073cf3974f84d948d92
YoussefSaybi <147932529+YoussefSaybi@users.noreply.github.com>
2023-10-14 17:14:31 +0100

    Add files via upload

---
+++ maintp1exer1.java
@@ -0,0 +1,10 @@
+public class maintp1exer1 {
+    public  static void main(String[] args)
+    {
+        tp1exer1 R1=new tp1exer1();
+        System.out.println(" la longeur 12 et le  largeur 9" + R1.diagonale(12,9));
+        System.out.println("la longeur 15 et la largeur 10" + R1.doubler(15,10));
+        System.out.println("la longeur 30 et de largeur 9" + R1.perimetre(30,9));
+
+    }
+}
---
+++ tp1exer1.java
@@ -0,0 +1,19 @@
+public class tp1exer1 {
+    float longeur;
+    float largeur;
+
+    public tp1exer1() {
+    }
+
+    float perimetre(float a, float b) {
+        return a * 2.0F + b * 2.0F;
+    }
+
+    float diagonale(float a, float b) {
+        return a * a + b * b;
+    }
+
+    float doubler(float a, float b) {
+        return a * 2.0F;
+    }
+}
